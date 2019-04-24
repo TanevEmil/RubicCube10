09addaa2a1b34f7c9e02c99d3430251a88803559
Admin <doxmoxbox@gmail.com>
2019-04-24 20:39:35 +0200

    Few colors.

--- app/src/main/java/com/example/rubiccube10/MainActivity.java
+++ app/src/main/java/com/example/rubiccube10/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.rubiccube10;
 
-import android.graphics.Color;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.drawable.ColorDrawable;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
